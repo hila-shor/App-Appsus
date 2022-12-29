1717779a53946a8597a6535944ebdedd0344be0b
Andrey Solonikov <andreysol@yahoo.com>
2022-12-29 08:29:18 +0200

    added buttons

--- apps/note/cmps/app-aside.jsx
+++ apps/note/cmps/app-aside.jsx
@@ -1,3 +1,11 @@
 export function AppAside() {
-  return <aside className='app-aside'>aside</aside>;
+  return (
+    <aside className='app-aside'>
+      <button className='btn btn-aside'>button</button>
+      <button className='btn btn-aside'>button</button>
+      <button className='btn btn-aside'>button</button>
+      <button className='btn btn-aside'>button</button>
+      <button className='btn btn-aside'>button</button>
+    </aside>
+  );
 }
