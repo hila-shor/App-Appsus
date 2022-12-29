7002b6dbdd8b89583ed499b77b7c35edcedd1fb6
Andrey Solonikov <andreysol@yahoo.com>
2022-12-29 07:58:14 +0200

    added some styles

--- services/util.service.js
+++ services/util.service.js
@@ -1,79 +1,123 @@
 export const utilService = {
-    makeId,
-    makeLorem,
-    getRandomIntInclusive,
-    getRandomColor,
-    padNum,
-    getDayName,
-    getMonthName,
-    convertTimestamp
-}
+  makeId,
+  makeLorem,
+  getRandomIntInclusive,
+  getRandomColor,
+  padNum,
+  getDayName,
+  getMonthName,
+  convertTimestamp,
+};
 
 function makeId(length = 6) {
-    var txt = ''
-    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  var txt = '';
+  var possible =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
-    for (var i = 0; i < length; i++) {
-        txt += possible.charAt(Math.floor(Math.random() * possible.length))
-    }
+  for (var i = 0; i < length; i++) {
+    txt += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
 
-    return txt
+  return txt;
 }
 
 function makeLorem(size = 100) {
-    var words = ['The sky', 'above', 'the port', 'was', 'the color of television', 'tuned', 'to', 'a dead channel', '.', 'All', 'this happened', 'more or less', '.', 'I', 'had', 'the story', 'bit by bit', 'from various people', 'and', 'as generally', 'happens', 'in such cases', 'each time', 'it', 'was', 'a different story', '.', 'It', 'was', 'a pleasure', 'to', 'burn']
-    var txt = ''
-    while (size > 0) {
-        size--
-        txt += words[Math.floor(Math.random() * words.length)] + ' '
-    }
-    return txt
+  var words = [
+    'The sky',
+    'above',
+    'the port',
+    'was',
+    'the color of television',
+    'tuned',
+    'to',
+    'a dead channel',
+    '.',
+    'All',
+    'this happened',
+    'more or less',
+    '.',
+    'I',
+    'had',
+    'the story',
+    'bit by bit',
+    'from various people',
+    'and',
+    'as generally',
+    'happens',
+    'in such cases',
+    'each time',
+    'it',
+    'was',
+    'a different story',
+    '.',
+    'It',
+    'was',
+    'a pleasure',
+    'to',
+    'burn',
+  ];
+  var txt = '';
+  while (size > 0) {
+    size--;
+    txt += words[Math.floor(Math.random() * words.length)] + ' ';
+  }
+  return txt;
 }
 
 function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min)
-    max = Math.floor(max)
-    return Math.floor(Math.random() * (max - min + 1)) + min //The maximum is inclusive and the minimum is inclusive 
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
 }
 
 function padNum(num) {
-    return (num > 9) ? num + '' : '0' + num
+  return num > 9 ? num + '' : '0' + num;
 }
 
 function getRandomColor() {
-    const letters = '0123456789ABCDEF'
-    var color = '#'
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)]
-    }
-    return color
+  const letters = '0123456789ABCDEF';
+  var color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
 
 function getDayName(date, locale) {
-    date = new Date(date)
-    return date.toLocaleDateString(locale, { weekday: 'long' })
+  date = new Date(date);
+  return date.toLocaleDateString(locale, { weekday: 'long' });
 }
 
-
 function getMonthName(date) {
-    const monthNames = ["January", "February", "March", "April", "May", "June",
-        "July", "August", "September", "October", "November", "December"
-    ]
-    return monthNames[date.getMonth()]
+  const monthNames = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  return monthNames[date.getMonth()];
 }
 
 function convertTimestamp(timestamp) {
-    const date = new Date(timestamp);
-    const currentDate = new Date();
-    const diffInDays = Math.floor((currentDate - date) / (1000 * 60 * 60 * 24));
-    if (diffInDays > 0) {
-        const month = date.toLocaleString('default', { month: 'long' });
-        return `${date.getDate()} ${month}, ${date.getHours()}:${date.getMinutes()}`;
-    }
-    const diffInHours = Math.floor((currentDate - date) / (1000 * 60 * 60));
-    if (diffInHours > 0) {
-        return `${date.getHours()}:${date.getMinutes()}`;
-    }
-    const diffInMinutes = Math.floor((currentDate - date) / (1000 * 60));
+  const date = new Date(timestamp);
+  const currentDate = new Date();
+  const diffInDays = Math.floor((currentDate - date) / (1000 * 60 * 60 * 24));
+  if (diffInDays > 0) {
+    const month = date.toLocaleString('default', { month: 'long' });
+    return `${date.getDate()} ${month}, ${date.getHours()}:${date.getMinutes()}`;
+  }
+  const diffInHours = Math.floor((currentDate - date) / (1000 * 60 * 60));
+  if (diffInHours > 0) {
     return `${date.getHours()}:${date.getMinutes()}`;
+  }
+  const diffInMinutes = Math.floor((currentDate - date) / (1000 * 60));
+  return `${date.getHours()}:${date.getMinutes()}`;
 }
